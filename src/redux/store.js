67f51ec82bf838c73fe8b93aae29413a808abe3c
DmitryDr-dev <dmitrydr.dev@gmail.com>
2021-11-25 12:02:56 +0200

    Add redux

---
+++ src/redux/store.js
@@ -0,0 +1,30 @@
+import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+import contactsReducers from './contacts-reducer';
+
+const store = configureStore({
+  reducer: contactsReducers,
+  devTools: process.env.NODE_ENV === 'development',
+});
+
+export default store;
+
+// import { composeWithDevTools } from 'redux-devtools-extension';
+
+// import contactsReducer from './contacts-reducer';
+
+// const persistedState = localStorage.getItem('reduxState')
+//   ? JSON.parse(localStorage.getItem('reduxState'))
+//   : {};
+
+// const store = createStore(
+//   contactsReducer,
+//   persistedState,
+//   composeWithDevTools(),
+// );
+
+// store.subscribe(() => {
+//   localStorage.setItem('reduxState', JSON.stringify(store.getState()));
+// });
+
+// export default store;
